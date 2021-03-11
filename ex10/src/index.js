d6acdd040c7ad799209847340adbead441e0c794
zahirbegulic@gmail.com <zahirbegulic@gmail.com>
2021-03-11 11:17:26 +0100

    Declared three string variables

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+
+    var myFirstName = "Zahir";
+    var myLastName = "Begulic";
+    var myHomeTown = "Velika Kladusa";
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+
+}
+console.log(main());
+module.exports = main;
